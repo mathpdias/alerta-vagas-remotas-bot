4391288093aba4608dd13bbcb0c97a87b4b5bb06
mathpdias <66956332+mathpdias@users.noreply.github.com>
2025-07-30 10:32:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-from telegram_bot import send_to_telegram
+import os
+import requests
 
-send_to_telegram("✅ Bot de vagas funcionando corretamente!")
+BOT_TOKEN = os.getenv("8070777170:AAEXhz786u288fZGkTEvElSjDwm2jz0f-pk")
+CHAT_ID = os.getenv("-1002311325037")
+
+def send_to_telegram(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": msg,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, data=payload)
+    print("STATUS:", response.status_code)
+    print("RESPOSTA:", response.text)
+
+send_to_telegram("✅ Teste direto pelo Python!")
