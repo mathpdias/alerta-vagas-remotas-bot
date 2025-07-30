892f7cddcd1b685047e1e80f0c07c831bb9b19df
mathpdias <66956332+mathpdias@users.noreply.github.com>
2025-07-30 09:54:40 -0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,19 @@
-import requestsimport osBOT_TOKEN = os.getenv("BOT_TOKEN")CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")sent_jobs = set()def send_to_telegram(message):    if message in sent_jobs:        return    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"    data = {        "chat_id": CHAT_ID,        "text": message,        "parse_mode": "Markdown"    }    requests.post(url, data=data)    sent_jobs.add(message)
+import requests
+import os
+
+BOT_TOKEN = os.getenv("8070777170:AAEXhz786u288fZGkTEvElSjDwm2jz0f-pk")
+CHAT_ID = os.getenv("-1002311325037")
+
+sent_jobs = set()
+
+def send_to_telegram(message):
+    if message in sent_jobs:
+        return
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+    requests.post(url, data=data)
+    sent_jobs.add(message)
